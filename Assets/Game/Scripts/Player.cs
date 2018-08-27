66aa0c5de76fd57279aa05e659468a3da139c9dc
Henrik Karlsson <p.h.o.karlsson@gmail.com>
2018-08-27 09:40:57 +0200

    Updating player movement

--- Assets/Game/Scripts/Player.cs
+++ Assets/Game/Scripts/Player.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-	public float speed = 5.0f;
+	[SerializeField] // Allows designer to change speed, while keeping it private to player (no other script can interfere with it)
+	private float speed = 5.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -13,9 +14,33 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		Movement();
+	}
+
+	// How the user control player movement
+	private void Movement () {
 		float horizontalInput = Input.GetAxis("Horizontal");
 		float verticalInput = Input.GetAxis("Vertical");
+
 		transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
 		transform.Translate(Vector3.up * speed * verticalInput * Time.deltaTime);
+
+		if (transform.position.y > 0)
+		{
+			transform.position = new Vector3(transform.position.x, 0, 0);
+		}
+		else if (transform.position.y < -4.2f)
+		{
+			transform.position = new Vector3(transform.position.x, -4.2f, 0);
+		}
+
+		if (transform.position.x > 10.6)
+		{
+			transform.position = new Vector3(-10.5f, transform.position.y, 0);
+		}
+		else if (transform.position.x < -10.6)
+		{
+			transform.position = new Vector3(10.5f, transform.position.y, 0);
+		}
 	}
 }
